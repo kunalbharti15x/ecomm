4add9e6f7213dfb6b0e27516c769830d209b56e6
Kunal Bharti <141907981+kunalbharti15x@users.noreply.github.com>
2023-12-10 18:59:31 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='footer-container'>
       <p>
-        2022 AD Headphones All rights reserved
+        2022 Kunal Headphones All rights reserved
       </p>
       <p className="icons">
         <AiFillInstagram/>
